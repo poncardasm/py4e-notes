f9f73daa023de401477a5e37beb5dc40d88e0185
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-03-07 01:55:01 +0200

    Add URL to documentation in prompt for user input program

--- 06-loops-and-iterations/ex_5_2.py
+++ 06-loops-and-iterations/ex_5_2.py
@@ -1,5 +1,7 @@
 """
 5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. Once 'done' is entered, print out the largest and smallest of the numbers. If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. Enter 7, 2, bob, 10, and 4 and match the output below. 
+
+https://www.py4e.com/tools/pythonauto/?PHPSESSID=fa0eaeade8b5d426587b03437148723d
 """
 largest = None
 smallest = None
